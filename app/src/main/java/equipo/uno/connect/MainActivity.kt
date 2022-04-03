9d115c6179a18e64aabe7569e205fdda0fd8af34
cristinaguilar333 <cristina.aguilar216559@potros.itson.edu.mx>
2022-04-03 15:53:56 -0700

    splash

--- app/src/main/java/equipo/uno/connect/MainActivity.kt
+++ app/src/main/java/equipo/uno/connect/MainActivity.kt
@@ -3,9 +3,15 @@ package equipo.uno.connect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Connect)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
