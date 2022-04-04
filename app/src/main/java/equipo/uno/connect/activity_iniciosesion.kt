c7e556ba6235d98c70af95bd4e855ff8110a9dd2
cristinaguilar333 <cristina.aguilar216559@potros.itson.edu.mx>
2022-04-03 21:53:05 -0700

    inicio sesión

---
+++ app/src/main/java/equipo/uno/connect/activity_iniciosesion.kt
@@ -0,0 +1,11 @@
+package equipo.uno.connect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_iniciosesion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iniciosesion)
+    }
+}
