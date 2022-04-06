6ae4ec40705f0656f05cbdfda5088f966cd544de
cristinaguilar333 <cristina.aguilar216559@potros.itson.edu.mx>
2022-04-06 00:02:52 -0700

    navegación perfil, conf y chat individual

--- app/src/main/java/equipo/uno/connect/configuracion.kt
+++ app/src/main/java/equipo/uno/connect/configuracion.kt
@@ -1,11 +1,19 @@
 package equipo.uno.connect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class configuracion : AppCompatActivity() {
+    val volver : ImageButton = findViewById(R.id.btnVolver1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracion)
+        
+        volver.setOnClickListener{
+            var intent : Intent = Intent(this,ChatActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/equipo/uno/connect/perfil.kt
+++ app/src/main/java/equipo/uno/connect/perfil.kt
@@ -1,11 +1,23 @@
 package equipo.uno.connect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 
 class perfil : AppCompatActivity() {
+    val volver : ImageButton = findViewById(R.id.btnVolver)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+
+        volver.setOnClickListener{
+            var intent : Intent = Intent(this,ChatActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
