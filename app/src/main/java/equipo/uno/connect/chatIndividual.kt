67e40ff8980d9a951aaa417e3edd570144a40022
cristinaguilar333 <cristina.aguilar216559@potros.itson.edu.mx>
2022-04-05 23:33:26 -0700

    chat

---
+++ app/src/main/java/equipo/uno/connect/chatIndividual.kt
@@ -0,0 +1,11 @@
+package equipo.uno.connect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class chatIndividual : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_individual)
+    }
+}
