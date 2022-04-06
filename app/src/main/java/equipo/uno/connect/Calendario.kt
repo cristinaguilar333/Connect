a8bd1c9ff55b36422e991355bc4dd4721abff8bc
Carmina Moreno <carminamoreno29@gmail.com>
2022-04-05 20:22:52 -0700

    test2

--- app/src/main/java/equipo/uno/connect/Calendario.kt
+++ app/src/main/java/equipo/uno/connect/Calendario.kt
@@ -2,10 +2,17 @@ package equipo.uno.connect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Calendario : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendario)
+
     }
+
+
 }
