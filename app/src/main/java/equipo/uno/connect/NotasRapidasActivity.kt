e5c707eab5f86fc7be3488e9a6d6a43e13c1128e
Carmina Moreno <carminamoreno29@gmail.com>
2022-05-16 16:06:47 -0700

    notas

--- app/src/main/java/equipo/uno/connect/NotasRapidasActivity.kt
+++ app/src/main/java/equipo/uno/connect/NotasRapidasActivity.kt
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 
 class NotasRapidasActivity : AppCompatActivity() {
     public final var REQUEST_CODE_NOTE : Int = 1
+    private val notaRef = FirebaseDatabase.getInstance().getReference("Nota")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +25,18 @@ class NotasRapidasActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        notaRef.addChildEventListener(object : ChildEventListener{
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                val nota = dataSnapshot.getValue(Nota::class.java)
+                if(nota != null) writeMark(nota)
+            }
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+            override fun onChildRemoved(snapshot: DataSnapshot) {}
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
+
 
         val volver : ImageButton = findViewById(R.id.btnVolverNotas)
         volver.setOnClickListener{
@@ -26,4 +44,13 @@ class NotasRapidasActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    private fun saveMarkFromForm(){
+
+    }
+    private fun writeMark(mark:Nota){
+        var listV : TextView = findViewById(R.id.textView3)
+        val text = listV.text.toString() + mark.toString() + "\n"
+        listV.text = text
+    }
+
 }
