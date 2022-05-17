610fe6b685e3fb923e552c40d8e7e6f1c9a52dd6
Carmina Moreno <carminamoreno29@gmail.com>
2022-05-16 18:58:10 -0700

    notaaa

--- app/src/main/java/equipo/uno/connect/NotasRapidasActivity.kt
+++ app/src/main/java/equipo/uno/connect/NotasRapidasActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -16,10 +17,19 @@ import kotlinx.android.synthetic.main.activity_notas_rapidas.*
 class NotasRapidasActivity : AppCompatActivity() {
     public final var REQUEST_CODE_NOTE: Int = 1
     private val notaRef = FirebaseDatabase.getInstance().getReference("connect")
+    private lateinit var empRecyclerView : RecyclerView
+    private lateinit var  tvLoadingData : TextView
+    lateinit var binding: ActivityNotasRapidasBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notas_rapidas)
+
+        empRecyclerView = findViewById(R.id.rv_notas)
+
+
+
+
 
         val addNote: ImageButton = findViewById(R.id.btn_AgregarNotaRapida)
         addNote.setOnClickListener {
@@ -27,21 +37,21 @@ class NotasRapidasActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-      
-        notaRef.addChildEventListener(object : ChildEventListener {
-            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
-                val nota = dataSnapshot.getValue(Nota::class.java)
-                if (nota != null) {
-                    writeMark(nota)
-                }
-            }
 
-            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
-            override fun onChildRemoved(snapshot: DataSnapshot) {}
-            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
-            override fun onCancelled(error: DatabaseError) {}
+     //   notaRef.addChildEventListener(object : ChildEventListener {
+       //     override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+         //       val nota = dataSnapshot.getValue(Nota::class.java)
+           //     if (nota != null) {
+             //       writeMark(nota)
+               // }
+            //}
 
-        })
+         //   override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+          //  override fun onChildRemoved(snapshot: DataSnapshot) {}
+          //  override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+          //  override fun onCancelled(error: DatabaseError) {}
+
+       // })
 
 
         val volver: ImageButton = findViewById(R.id.btnVolverNotas)
@@ -51,13 +61,6 @@ class NotasRapidasActivity : AppCompatActivity() {
         }
     }
 
-    private fun writeMark(mark: Nota) {
-        var listV: TextView = findViewById(R.id.list_textView) as TextView
-        val text = listV.text.toString() + mark.toString() + "\n"
-        //listV.text = text
-        list_textView.setText(text.toString())
 
-
-    }
 
 }
