9a893b915af69a17b167417ea02cef338a40d7f6
Carmina Moreno <carminamoreno29@gmail.com>
2022-05-16 20:58:59 -0700

    perfil

--- app/src/main/java/equipo/uno/connect/perfil.kt
+++ app/src/main/java/equipo/uno/connect/perfil.kt
@@ -3,6 +3,7 @@ package equipo.uno.connect
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import kotlinx.android.synthetic.main.activity_perfil.*
 
@@ -10,10 +11,14 @@ class perfil : AppCompatActivity() {
 
 
     lateinit var volver : ImageButton
+    lateinit var etUsuario : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
 
         val nombreUsuario = intent.getStringExtra("nombreUsuario")
+
+        etUsuario = findViewById(R.id.etUsuario)
 
         etUsuario.setText(nombreUsuario.toString())
 
